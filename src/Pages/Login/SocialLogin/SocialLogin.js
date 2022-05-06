e4d4ceb0c4c8fdc1c17256ff14d2ad618b96ec72
Yeasir-Hossain <yeasir06@gmail.com>
2022-05-07 00:02:28 +0600

    completed

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -4,17 +4,20 @@ import facebook from '../../../images/social/facebook.png';
 import github from '../../../images/social/github.png';
 import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Loading from '../../Shared/Loading/Loading';
 
 const SocialLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
     const navigate = useNavigate();
-    
+    const location = useLocation();
+
+    let from = location.state?.from?.pathname || "/";
+
     let errorElement;
 
-    if(loading || loading1){
+    if (loading || loading1) {
         return <Loading></Loading>
     }
 
@@ -22,10 +25,10 @@ const SocialLogin = () => {
         errorElement = <p className='text-danger'>Error: {error?.message} {error1?.message}</p>
     }
 
-    if (user || user1) {
-        navigate('/home');
-    }
 
+    if (user || user1) {
+        navigate(from, { replace: true });
+    }
     return (
         <div>
             <div className='d-flex align-items-center'>
---
+++ src/Pages/Order/Order.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import axiosPrivate from '../../api/axiosPrivate';
+
+const Order = () => {
+    const [user] = useAuthState(auth)
+    const [orders, setOrders] = useState([])
+    const navigate = useNavigate()
+    useEffect(()=>{
+        const getOrders = async()=>{
+            const email = user.email;
+            const url =`http://localhost:5000/order?email=${email}`
+           try{
+            const {data} = await axiosPrivate.get(url);
+            setOrders(data);
+           }
+           catch(error){
+               console.log(error);
+               if(error.response.status === 403 || error.response.status === 401){
+                   signOut(auth)
+                navigate('/login')
+               }
+           }
+        }
+        getOrders();
+    },[user])
+    return (
+        <div>
+            <h2>Your orders: {orders.length}</h2>
+        </div>
+    );
+};
+
+export default Order;
